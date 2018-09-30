4e045e031970b8f9bd7053551f80e4aa73bd25d6
Marsnaut <jhuang5132@gmail.com>
2018-09-30 00:39:35 -0400

    initial commit - basic implementation

---
+++ src/components/Fab.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Fab extends Component {
+  render() {
+    return (
+    <div className="open-search">
+      <Link to={"/search"}>Add a Book</Link>
+    </div>
+    );
+  }
+}
