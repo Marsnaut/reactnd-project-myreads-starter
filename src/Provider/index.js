a0417ff959334ee9ad199841ae519dada77be7c1
Marsnaut <jhuang5132@gmail.com>
2018-10-06 02:17:54 -0400

    fixed issue where moving the book to 'none' didn't update the state

--- src/Provider/index.js
+++ src/Provider/index.js
@@ -16,16 +16,29 @@ export default class index extends Component {
         this.setState({ books, currentlyReading, read, wantToRead });
       },
       moveBook: (book, newShelf, allShelfs) => {
-        const newBooks = this.state.books.map(allBooks => {
-          const foundID = allShelfs[newShelf].find(
-            bookID => bookID === allBooks.id
-          );
-          if (foundID) {
-            allBooks.shelf = newShelf;
-          }
-          return allBooks;
-        })
-        this.state.addBooks(newBooks);
+        // if moving
+        try {
+          const newBooks = this.state.books.map(allBooks => {
+            const foundID = allShelfs[newShelf].find(
+              bookID => bookID === allBooks.id
+            );
+            if (foundID) {
+              allBooks.shelf = newShelf;
+            }
+            return allBooks;
+          })
+          this.state.addBooks(newBooks);
+        
+        // if moving to none
+        } catch(error) {
+          const books = this.state.books.filter(bookInside => {
+            return bookInside.title !== book.title
+          })
+        const currentlyReading = books.filter(book => book.shelf === 'currentlyReading');
+        const read = books.filter(book => book.shelf === 'read');
+        const wantToRead = books.filter(book => book.shelf === 'wantToRead');
+        this.setState({ books, currentlyReading, read, wantToRead });
+        }
       }
     }
   }
